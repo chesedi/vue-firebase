e993ac0d1ba228511bda6fc849bce5daa7df2985
chesedi <lp90553@linecorp.com>
2020-01-21 11:50:13 +0900

    add toolbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
-    abcdefg
+    <v-alert type="primary" :value="true" v-for="i in array" :key="i">
+      adcd {{i}}
+    </v-alert>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      seen: false,
+      message: 'abcbindd',
+      array: [1, 2, 3, 4, 'xx']
+    }
+  }
+}
+</script>
