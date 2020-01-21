f5366701f7cf4cfcf6f5d5327e53093cf0e56292
chesedi <lp90553@linecorp.com>
2020-01-21 16:51:49 +0900

    add CRUD func

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,23 +2,14 @@ import Vue from 'vue'
 import * as firebase from 'firebase/app'
 import firebaseConfig from '../../firebaseConfig'
 
-import 'firebase/auth'
+// import 'firebase/auth'
 import 'firebase/firestore'
-
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyB9FLSHSGDfohMlR03BrkshH28p9HQ1mRQ',
-//   authDomain: 'test-vf-1-8094a.firebaseapp.com',
-//   databaseURL: 'https://test-vf-1-8094a.firebaseio.com',
-//   projectId: 'test-vf-1-8094a',
-//   storageBucket: 'test-vf-1-8094a.appspot.com',
-//   messagingSenderId: '928881654915',
-//   appId: '1:928881654915:web:610970e31f5dd2f08a2fb6',
-//   measurementId: 'G-18RPZ1CZKR'
-// }
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-firebase.firestore().collection('test').add({ test: 'aaa' })
-  .then(r => console.log(r))
-  .catch(e => console.error(e))
+// firebase.firestore().collection('test').add({ test: 'aaa' })
+//   .then(r => console.log(r))
+//   .catch(e => console.error(e))
+
+Vue.prototype.$firebase = firebase
