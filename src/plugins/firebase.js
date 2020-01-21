755d18e9d90c2b5df6d39c059ac27e44ebca6274
chesedi <lp90553@linecorp.com>
2020-01-21 17:21:01 +0900

    add auth

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import * as firebase from 'firebase/app'
 import firebaseConfig from '../../firebaseConfig'
 
-// import 'firebase/auth'
+import 'firebase/auth'
 import 'firebase/firestore'
 
 // Initialize Firebase
