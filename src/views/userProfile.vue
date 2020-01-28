fc2f5e8c3c10fb242d6f3c046e18f744096ba9ab
chesedi <lp90553@linecorp.com>
2020-01-28 12:29:22 +0900

    add userInfo to firestore

--- src/views/userProfile.vue
+++ src/views/userProfile.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <p>userProfile page</p>
-    {{$store.state.claims.level}}
+    <p>user profile</p>
+    {{ JSON.stringify($store.state.claims, null, 2) }}
+    <v-btn color="success" @click="tokenUpdate">tokenUpdate</v-btn>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    async tokenUpdate () {
+      const user = this.$firebase.auth().currentUser
+      await user.getIdToken(true)
+      await this.$store.dispatch('getUser', user)
+    }
+  }
+}
+</script>
