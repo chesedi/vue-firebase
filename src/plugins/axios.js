f5d488142489ef049682529bffbdd2738424ee48
chesedi <lp90553@linecorp.com>
2020-01-29 12:53:24 +0900

    firebase hosting deploy

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import axios from 'axios'
 import store from '../store'
+import firebaseConfig from '../../firebaseConfig'
 
 const firebaseApi = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://us-central1-test-vf-1-8094a.cloudfunctions.net/' : 'http://localhost:5000/test-vf-1-8094a/us-central1/',
-  timeout: 5000,
-  headers: { 'X-Custom-Header': 'foobar' }
+  baseURL: process.env.NODE_ENV === 'production' ? `https://us-central1-${firebaseConfig.projectId}.cloudfunctions.net/` : `http://localhost:5000/${firebaseConfig.projectId}/us-central1/`,
+  timeout: 5000
 })
 
 firebaseApi.interceptors.request.use(function (config) {
